8c7a1cc4f092ec175fb5066604a89dbf53338f03
subhash dangi <166481929+Subhashdangi205@users.noreply.github.com>
2025-07-13 16:26:51 +0530

    Added STATIC_ROOT for deployment

--- FLIPR/FLIPR/settings.py
+++ FLIPR/FLIPR/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -132,3 +134,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",]
+
